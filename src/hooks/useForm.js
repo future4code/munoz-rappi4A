45196377187b30f28bf4da11c4ca39111d81c101
Clara Meirelles <clara_meirelles@hotmail.com>
2021-07-21 17:22:28 -0300

    criação dos custom hooks useRequesData e useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export const useForm = (initialState) => {
+    const [form, setForm] = useState(initialState)
+
+    const onChangeForm = (e) => {
+        const {name, value} = e.target
+        setForm[{...form, [name]: value}]
+    }
+
+    const clearInputs = () => {
+        setForm(initialState)
+    }
+
+    return{form, onChangeForm, clearInputs}
+
+}
