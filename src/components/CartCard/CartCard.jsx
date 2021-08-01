caf7a820d6b7f3760ec9f6da2d21bd64dd793ad8
Juliana Moraes <juliana.hmoraes@gmail.com>
2021-08-01 19:46:32 -0300

    correcao props cartCard.jsx

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -3,16 +3,16 @@ import { formatPrice } from '../../utils/formatPrice'
 import { Card, ImageContainer, InfoBox, ProductDescription, ProductPrice, ProductTitle, QuantityBox, RemoveButton } from './styled'
 
 
-export const CartCard = ({product}) => {
+export const CartCard = ({product, removeItemFromCart}) => {
   return (
     <Card key={product.id}>
       <ImageContainer image={product.photoUrl} />
       <InfoBox>
-        <ProductTitle>{props.product.name}</ProductTitle>
-        <ProductDescription>{props.product.description}</ProductDescription>
-        <ProductPrice>{formatPrice(props.product.price)}</ProductPrice>
-        <QuantityBox>{props.product.quantity}</QuantityBox>
-        <RemoveButton onClick={() => props.removeItemFromCart(props.product.id)}>remover</RemoveButton>
+        <ProductTitle>{product.name}</ProductTitle>
+        <ProductDescription>{product.description}</ProductDescription>
+        <ProductPrice>{formatPrice(product.price)}</ProductPrice>
+        <QuantityBox>{product.quantity}</QuantityBox>
+        <RemoveButton onClick={() => removeItemFromCart(product.id)}>remover</RemoveButton>
       </InfoBox>
     </Card>
   )
