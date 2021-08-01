b197040db39125cada4c05a6a7a7fe396419b9c6
Rafael S. da Rosa <rafaelsilveeira@users.noreply.github.com>
2021-07-31 22:04:19 -0300

    Página Carrinho e Card de produto

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Card, ImageContainer, InfoBox, QuantityBox, RemoveButton } from './styled'
+import { formatPrice } from '../../utils/formatPrice'
+import { Card, ImageContainer, InfoBox, ProductDescription, ProductPrice, ProductTitle, QuantityBox, RemoveButton } from './styled'
+
 
 export const CartCard = (props) => {
   return (
     <Card key={props.product.id}>
       <ImageContainer image={props.product.photoUrl} />
       <InfoBox>
-        <QuantityBox>2</QuantityBox>
-        <RemoveButton>remover</RemoveButton>
+        <ProductTitle>{props.product.name}</ProductTitle>
+        <ProductDescription>{props.product.description}</ProductDescription>
+        <ProductPrice>{formatPrice(props.product.price)}</ProductPrice>
+        <QuantityBox>{props.product.quantity}</QuantityBox>
+        <RemoveButton onClick={() => props.removeItemFromCart(props.product.id)}>remover</RemoveButton>
       </InfoBox>
     </Card>
   )
