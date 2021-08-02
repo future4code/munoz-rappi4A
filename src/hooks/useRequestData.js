34afe3623839d5f65a730d31bbf93bafe0219684
Brendha Luiza Barbosa Pereira <barbosabrendhaluiza@gmail.com>
2021-08-02 20:53:13 -0300

    filtro por tipos de comida

--- src/hooks/useRequestData.js
+++ src/hooks/useRequestData.js
@@ -18,7 +18,7 @@ export const useRequestData = (path, token) => {
         setLoading(false);
       })
       .catch((err) => {
-        console.log(err.response.data);
+        console.log(err.response);
         setLoading(false);
       });
   }, [path]);
